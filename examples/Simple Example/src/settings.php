745ef535bd554fe44b4586ec70e7cc8de373e2b4
micahbaumann <78232761+micahbaumann@users.noreply.github.com>
2022-10-12 01:07:02 -0700

    Update and rename examples/Simple Example/scr/settings.php to examples/Simple Example/src/settings.php

---
+++ examples/Simple Example/src/settings.php
@@ -0,0 +1,16 @@
+<?php
+/*
+URLS framework url config file.
+
+Add your paths here:
+ex. $urls->path('blog/', 'blog-home.php', true);
+*/
+include 'urls/Urls.php';
+Urls::$base = '/';
+
+$urls = new Urls;
+$urls->path('blog/', 'blog-home.php', true);
+
+$urls->exe();
+
+?>
